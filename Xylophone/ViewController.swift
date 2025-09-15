f32e9df9f39de65eace50758c2171d5e27124b5b
waseem <waseemabbaskpr786@gmail.com>
2025-09-15 04:48:32 -0700

    paractice

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let title = sender.currentTitle else { return }
+        switch title {
+        case "C" :
+            playSound(name: "C")
+        case "D" :
+            playSound(name: "D")
+        case "E" :
+            playSound(name: "E")
+        case "F" :
+            playSound(name: "F")
+        case "G" :
+            playSound(name: "G")
+        case "A" :
+            playSound(name: "A")
+        case "B" :
+            playSound(name: "B")
+        default:
+            break
+        }
+       
+    }
+   
     
-
+    
+    
+    func playSound(name : String) {
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
